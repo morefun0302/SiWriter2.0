7237ab7a200dc295da62730b045417ecfc59b587
Simon Anthony <git@torty.org.uk>
2014-11-18 12:49:51 +1100

    Android and iphone tests

--- build/iphone/build/SiWriterPro.build/Debug-iphoneos/SiWriterPro-iPad.build/Objects-normal/armv7/CAXException.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/Cyberman/Documents/Titanium_Studio_Workspace/SiWriterPro1.5.9.7/build/iphone/Classes/CAXException.cpp \
-  /Users/Cyberman/Documents/Titanium_Studio_Workspace/SiWriterPro1.5.9.7/build/iphone/Classes/CAXException.h \
-  /Users/Cyberman/Documents/Titanium_Studio_Workspace/SiWriterPro1.5.9.7/build/iphone/Classes/CADebugMacros.h
